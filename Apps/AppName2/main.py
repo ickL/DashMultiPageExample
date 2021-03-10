8beb5c223cdafe15eb57ace6db862e53a976d3ac
ickL <53160568+ickL@users.noreply.github.com>
2021-03-10 14:25:44 -0500

    Update main.py
    App Name will be derived from App folder name through file name lookup

--- Apps/AppName2/main.py
+++ Apps/AppName2/main.py
@@ -1,8 +1,8 @@
 import importlib
 import DashboardLayout
+import os
 
-
-Name = "AppName2"
+Name = os.path.dirname(os.path.realpath(__file__)).partition("Apps\\")[-1]
 print("Loading " + Name)
 Page = importlib.import_module("Apps." + Name + ".DashboardLayout").layout
 Callbacks = importlib.import_module("Apps." + Name + ".callbacks")
